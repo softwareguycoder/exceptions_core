36ffbc63c6cf3398b8c6fc73474824d8fc12e60d
softwareguycoder <softwareguycoder@gmail.com>
2019-06-10 09:16:45 -0400

    Create exceptions_core_tests_symbols.h

---
+++ exceptions_core_tests/include/exceptions_core_tests_symbols.h
@@ -0,0 +1,10 @@
+// exceptions_core_tests_symbols.h - Defines constants and symbols for the
+// functions in the exceptions_core_tests library.
+//
+
+#ifndef __EXCEPTIONS_CORE_TESTS_SYMBOLS_H__
+#define __EXCEPTIONS_CORE_TESTS_SYMBOLS_H__
+
+
+
+#endif //__EXCEPTIONS_CORE_TESTS_SYMBOLS_H__
